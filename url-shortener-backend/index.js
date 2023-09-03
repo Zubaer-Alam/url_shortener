94ae36dd6dafae58d8f9b6cc7d22f7fbabb2cca2
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-03 14:32:42 +0600

    Code Refactoring, Bug-Fixes

--- url-shortener-backend/index.js
+++ url-shortener-backend/index.js
@@ -1,39 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const shortid = require('short-id');
-const UrlMapping = require('./models/urlMapping');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const shortid = require("short-id");
+const UrlMapping = require("./models/urlMapping");
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json({ extended: false }));
 
 const PORT = 8000;
 
 // Connect to MongoDB
 mongoose
-  .connect('mongodb://mongodb:27017/links', {
+  .connect("mongodb://mongodb:27017/links", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.error('MongoDB connection error:', err);
+    console.error("MongoDB connection error:", err);
   });
 
 // Route to handle URL mapping creation
 app.post('/shorten', async (req, res) => {
   const { longUrl } = req.body;
-
-  // Generate a short key using shortid
   const shortKey = shortid.generate();
+
+  // Ensure the original URL includes the protocol
+  const originalUrl = longUrl.startsWith('http') ? longUrl : `http://${longUrl}`;
 
   // Create a new URL mapping document
   const newUrlMapping = new UrlMapping({
     shortKey: shortKey,
-    originalUrl: longUrl,
+    originalUrl: originalUrl, // Save the complete URL
   });
 
   try {
@@ -46,23 +47,23 @@ app.post('/shorten', async (req, res) => {
 });
 
 // Route to get the mapped long key
-app.get('/:shortKey', async (req, res) => {
-    const { shortKey } = req.params;
-  
-    try {
-      const urlMapping = await UrlMapping.findOne({ shortKey });
-      if (urlMapping) {
-        // Redirect to the original URL
-        res.redirect(urlMapping.originalUrl);
-      } else {
-        // Handle when the short key is not found
-        res.status(404).send('Short key not found');
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-    }
-  });
-  
+app.get("/:shortKey", async (req, res) => {
+  const { shortKey } = req.params;
 
-app.listen(PORT, () => console.log('app is running'));
+  try {
+    const urlMapping = await UrlMapping.findOne({ shortKey });
+    if (urlMapping) {
+      // Redirect to the original URL
+      console.log(urlMapping.originalUrl);
+      res.redirect(urlMapping.originalUrl);
+    } else {
+      // Handle when the short key is not found
+      res.status(404).send("Short key not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.listen(PORT, () => console.log("app is running"));
--- url-shortener-frontend/src/UrlShortener.svelte
+++ url-shortener-frontend/src/UrlShortener.svelte
@@ -1,73 +1,72 @@
 <script>
-    let longUrl = '';
-    let shortUrl = '';
-    let showShortUrl = false;
-  
-    async function shortenUrl() {
-      const response = await fetch("http://localhost:8000/shorten", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ longUrl }),
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        shortUrl = data.shortKey;
-        showShortUrl = true;
-      } else {
-        console.error("Error:", response.status);
-      }
+  let longUrl = "";
+  let shortUrl = "";
+  let showShortUrl = false;
+
+  async function shortenUrl() {
+    const response = await fetch("http://localhost:8000/shorten", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ longUrl }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      shortUrl = data.shortKey;
+      showShortUrl = true;
+    } else {
+      console.error("Error:", response.status);
     }
-  </script>
-  
-  <main>
-    <h1>URL Shortener</h1>
-    <form>
-      <input type="text" bind:value={longUrl} placeholder="Enter a long URL">
-      <button type="button" on:click={shortenUrl}>Shorten</button>
-    </form>
-    {#if showShortUrl}
-      <div>
-        <p>Short URL: localhost:8000/{shortUrl}</p>
-        <a href={`http://localhost:8000/${shortUrl}`}>Go to Short URL</a>
-      </div>
-    {/if}
-  </main>
-  
-  <style>
-    main {
-      font-family: Arial, sans-serif;
-      text-align: center;
-      margin-top: 20px;
-    }
-  
-    h1 {
-      margin-bottom: 20px;
-    }
-  
-    form {
-      margin-bottom: 20px;
-    }
-  
-    input {
-      padding: 10px;
-    }
-  
-    button {
-      padding: 10px;
-      background-color: #007bff;
-      color: white;
-      border: none;
-      cursor: pointer;
-    }
-  
-    a {
-      color: #007bff;
-      text-decoration: none;
-      margin-top: 10px;
-      display: block;
-    }
-  </style>
-  
+  }
+</script>
+
+<main>
+  <h1>URL Shortener</h1>
+  <form>
+    <input type="text" bind:value={longUrl} placeholder="Enter a long URL" />
+    <button type="button" on:click={shortenUrl}>Shorten</button>
+  </form>
+  {#if showShortUrl}
+    <div>
+      <p>Short URL: localhost:8000/{shortUrl}</p>
+      <a href={`http://localhost:8000/${shortUrl}`}>Go to Short URL</a>
+    </div>
+  {/if}
+</main>
+
+<style>
+  main {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  h1 {
+    margin-bottom: 20px;
+  }
+
+  form {
+    margin-bottom: 20px;
+  }
+
+  input {
+    padding: 10px;
+  }
+
+  button {
+    padding: 10px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+
+  a {
+    color: #007bff;
+    text-decoration: none;
+    margin-top: 10px;
+    display: block;
+  }
+</style>
